47976d0c39ce315c8fc79fa29371ceea5da6ee92
Smith0314 <loyeu314@gmail.com>
2015-03-04 15:05:13 +0800

    Initial Commit

---
+++ Set-TextField-Height/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Set-TextField-Height
+//
+//  Created by Smith on 2015/3/4.
+//  Copyright (c) 2015年 Smith-Lab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
